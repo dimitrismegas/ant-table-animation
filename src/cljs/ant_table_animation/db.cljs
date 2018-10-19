16cf6c64f5ed22a6a8e3d8fa7f07cb54ea676efb
Dimitris Megas <megasd21@yahoo.gr>
2018-10-19 10:39:11 +0300

    db: initialize db with some orders

--- src/cljs/ant_table_animation/db.cljs
+++ src/cljs/ant_table_animation/db.cljs
@@ -1,4 +1,13 @@
 (ns ant-table-animation.db)
 
 (def default-db
-  {:name "re-frame"})
+  {:name "re-frame"
+   :orders [{:product "Monitor"
+            :quantity 500
+            :price 150}
+           {:product "Keyboard"
+            :quantity 120
+            :price 20}
+           {:product "Laptop"
+            :quantity 280
+            :price 599}]})
