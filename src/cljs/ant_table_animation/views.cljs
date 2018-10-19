3ae84368350024a91d3a8da87af0d791ff4320d5
Dimitris Megas <megasd21@yahoo.gr>
2018-10-19 11:03:25 +0300

    views: display orders in ant table

--- src/cljs/ant_table_animation/views.cljs
+++ src/cljs/ant_table_animation/views.cljs
@@ -2,10 +2,25 @@
   (:require
    [re-frame.core :as re-frame]
    [ant-table-animation.subs :as subs]
+   [antizer.reagent :as ant]
    ))
+
+(defn orders
+  []
+  (let [orders @(re-frame/subscribe [::subs/orders])
+        width 80]
+    [ant/table
+     {:columns [{:title "Product Name" :dataIndex :product :width width}
+                {:title "Quantity" :dataIndex :quantity :width width}
+                {:title "Unit Price" :dataIndex :price :width width}]
+      :dataSource orders
+      :size "small"
+      :pagination {:page-size 20}
+      :scroll {:y 300}}]))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
      [:h1 "Hello from " @name]
+     [orders]
      ]))
