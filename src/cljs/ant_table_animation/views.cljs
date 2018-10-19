5368ad71da58e005b70321e447aa243580425063
Dimitris Megas <megasd21@yahoo.gr>
2018-10-19 11:24:40 +0300

    views: add Actions column to orders' table
    Has button to delete order

--- src/cljs/ant_table_animation/views.cljs
+++ src/cljs/ant_table_animation/views.cljs
@@ -2,7 +2,9 @@
   (:require
    [re-frame.core :as re-frame]
    [ant-table-animation.subs :as subs]
+   [ant-table-animation.events :as events]
    [antizer.reagent :as ant]
+   [reagent.core :as reagent]
    ))
 
 (defn orders
@@ -10,9 +12,18 @@
   (let [orders @(re-frame/subscribe [::subs/orders])
         width 80]
     [ant/table
-     {:columns [{:title "Product Name" :dataIndex :product :width width}
-                {:title "Quantity" :dataIndex :quantity :width width}
-                {:title "Unit Price" :dataIndex :price :width width}]
+     {:columns
+      [{:title "Product Name" :dataIndex :product :width width}
+       {:title "Quantity" :dataIndex :quantity :width width}
+       {:title "Unit Price" :dataIndex :price :width width}
+       {:title "Actions" :dataIndex "actions" :width width
+        :render
+        #(reagent/as-element
+          [ant/button
+           {:icon "delete" :type "danger"
+            :on-click
+            (fn []
+              (re-frame/dispatch [::events/order-deleted (.-product %2)]))}])}]
       :dataSource orders
       :size "small"
       :pagination {:page-size 20}
